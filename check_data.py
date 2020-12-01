5d9ebaf74ede6d25132b66f6f7156da599ca23a6
olegush <oleg107117@gmail.com>
2020-12-01 22:33:45 +0300

    almost done

--- check_data.py
+++ check_data.py
@@ -1,16 +1,32 @@
 import os
 import json
+import jsonschema
+from jsonschema import validate, Draft3Validator
 
 
-def check_data(path):
-    schemas = dict()
-    with os.scandir(path) as entries:
+def check_data(path_schema, path_event):
+    schemas = []
+    with os.scandir(path_schema) as entries:
         for entry in entries:
-            with open(os.path.join(path, entry.name)) as f:
-                schemas[entry.name] = f.read()
-    for name, data in schemas.items():
-        print(name, json.loads(data))
+            with open(os.path.join(path_schema, entry.name)) as file:
+                schema = json.load(file)
+                schemas.append(dict(schema_file=entry.name, schema=schema))
+
+    log = []
+    for schema in schemas:
+        with os.scandir(path_event) as entries:
+            for entry in entries:
+                with open(os.path.join(path_event, entry.name)) as f:
+                    data = json.load(f)
+                    try:
+                        Draft3Validator(schema['schema']).validate(data)
+                    except jsonschema.exceptions.ValidationError as err:
+                        log.append(dict(file=entry.name, schema_file=schema['schema_file'], err=err))
+    for l in log:
+        print(l)
 
 
 if __name__ == '__main__':
-    check_data('task_folder/schema')
+    check_data('task_folder/schema', 'task_folder/event/')
+
+
